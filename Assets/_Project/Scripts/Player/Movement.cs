622d962f9982af203c9fd3e3004ec71cdaa5d001
Agustín Villanueva <agustin@bizbox.mx>
2020-04-26 18:20:20 -0500

    player movement

---
+++ Assets/_Project/Scripts/Player/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Movement : MonoBehaviour
+{
+    private GameObject player;
+
+    public Text moveText;
+    
+    void Start()
+    {
+        init();
+    }
+
+    void init()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    public void showText()
+    {
+        moveText.gameObject.SetActive(true);
+    }
+
+    public void hideText()
+    {
+        moveText.gameObject.SetActive(false);
+    }
+
+    public void movePlayer()
+    {
+        player.transform.position = new Vector3(gameObject.transform.position.x, 1, gameObject.transform.position.z);
+    }
+}

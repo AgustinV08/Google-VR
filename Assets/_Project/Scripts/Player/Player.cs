6d522285ffcbc2f05d4fda559adfa49b3347fd61
Agustín Villanueva <agustin@bizbox.mx>
2020-05-10 14:50:10 -0500

    added menu functionality

--- Assets/_Project/Scripts/Player/Player.cs
+++ Assets/_Project/Scripts/Player/Player.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+    public int health = 5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,24 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(1);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        SceneManager.LoadScene("Finish");
     }
 }
---
+++ Assets/_Project/Scripts/UI/Menu.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public float timeout = 2.0f;
+    
+    private Text timeoutText;
+
+    private bool play = false;
+    private bool quit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeoutText = GameObject.Find("Timeout").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (play || quit)
+        {
+            timeout -= Time.deltaTime;
+            
+            timeoutText.text = timeout.ToString("0.##");
+
+            if (timeout <= 0.0f)
+            {
+                if (play)
+                {
+                    Play();
+                }
+
+                if (quit)
+                {
+                    Quit();
+                }
+
+                timeout = 2.0f;
+            }
+        }
+    }
+
+    public void StartPlay()
+    {
+        play = true;
+        timeout = 2.0f;
+        
+        timeoutText.gameObject.SetActive(true);
+    }
+    
+    public void StartQuit()
+    {
+        play = true;
+        timeout = 2.0f;
+        
+        timeoutText.gameObject.SetActive(true);
+    }
+
+    public void StopLooking()
+    {
+        play = false;
+        quit = false;
+        timeout = 2.0f;
+        
+        timeoutText.gameObject.SetActive(false);
+    }
+
+    void Play()
+    {
+        SceneManager.LoadScene("Dev");
+    }
+
+    void Quit()
+    {
+        Application.Quit();
+    }
+}

51c81e56c964ed37c58763814b9aa8fc99519d00
Agustín Villanueva <agustin@bizbox.mx>
2020-05-11 20:09:54 -0500

    added win prefab and script

---
+++ Assets/_Project/Scripts/Player/Win.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+    private GameObject player;
+    private GameObject moveText;
+
+    private Text timeoutText;
+
+    public float timeout = 2.0f;
+
+    private bool lookingObject = false;
+
+    void Start()
+    {
+        init();
+    }
+
+    void Update()
+    {
+        if (lookingObject)
+        {
+            timeout -= Time.deltaTime;
+
+            timeoutText.text = timeout.ToString("0.##");
+
+            if (timeout <= 0.0f)
+            {
+                movePlayer();
+                
+                hideText();
+            }
+        }
+    }
+
+    void init()
+    {
+        player = GameObject.Find("Player");
+        moveText = GameObject.Find("Movement");
+        
+        timeoutText = GameObject.Find("MovementTimeoutText").GetComponent<Text>();
+        
+        Invoke("hideText", 0.01f);
+    }
+
+    public void showText()
+    {
+        moveText.SetActive(true);
+
+        lookingObject = true;
+    }
+
+    public void hideText()
+    {
+        moveText.SetActive(false);
+
+        lookingObject = false;
+
+        timeout = 2.0f;
+    }
+
+    public void movePlayer()
+    {
+        SceneManager.LoadScene("Win");
+    }
+}
